900cb5d9f8d13c56b210b49b16dd2b567894e729
Nathan C. McGuire <nathanmcguire@users.noreply.github.com>
2023-02-12 23:29:00 -0600

    Update main.py
    testing curses

--- main.py
+++ main.py
@@ -14,22 +14,36 @@ def main(stdscr):
     GREEN_AND_BLACK = curses.color_pair(2)
     RED_AND_WHITE = curses.color_pair(3)
 
+    counter_win = curses.newwin(1, 20, 10, 10)
+
     stdscr.clear()
     # stdscr.addstr(1, 1, " Mosyle Manager ", RED_AND_WHITE | curses.A_BOLD)
     # stdscr.refresh()
     # stdscr.getch()
 
-    for i in range(100):
-        stdscr.clear()
-        color = BLUE_AND_YELLOW
-        if i % 3 == 1:
-            color = GREEN_AND_BLACK
-        if i % 3 == 2:
-            color = RED_AND_WHITE
+    stdscr.addstr("Hello Worlds! ", RED_AND_WHITE | curses.A_BOLD)
+    stdscr.refresh()
+    #for i in range(100):
+    #    counter_win.clear()
+    #    color = BLUE_AND_YELLOW
+    #    if i % 3 == 1:
+    #        color = GREEN_AND_BLACK
+    #    if i % 3 == 2:
+    #        color = RED_AND_WHITE
 
-        stdscr.addstr(1, 1, f" Count: {i} ", color | curses.A_BOLD)
-        stdscr.refresh()
-        time.sleep(0.1)
+    #    counter_win.addstr(f" Count: {i} ", color | curses.A_BOLD)
+    #    counter_win.refresh()
+    #    time.sleep(0.1)
+        
+    pad = curses.newwin(100,100)
+    stdscr.refresh()
+    for i in range (100):
+        for j in range (26):
+            char = chr(67 + j)
+            pad.addstr(char, GREEN_AND_BLACK)
+    pad.refresh(0,0,5,5,10,75)
+    stdscr.refresh()
+    
     stdscr.getch()
 
 
